fc0007fff44014530b4484d6764f563bb9ce6378
ganesh <pilli.ganesh@unisys.com>
2019-10-11 11:54:50 +0530

    first commit

---
+++ de/src/de/Demo.java
@@ -0,0 +1,13 @@
+package de;
+
+public class Demo {
+	
+		public static void main(String ar[])
+	{
+	int a = 4 ;
+	int b = 5;
+	int c = a+b;
+	System.out.println(c);
+
+}
+	}
---
+++ de/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author DevOpsU9
+ *
+ */
+module de {
+}
